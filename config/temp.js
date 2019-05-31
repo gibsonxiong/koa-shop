f84b83ae8ad4663b65e0e6dac33ba0d075fe6760
gibsonxiong <442760763@qq.com>
2019-05-31 14:35:49 +0800

    config

---
+++ config/temp.js
@@ -0,0 +1,23 @@
+exports.port = '6868';
+
+exports.db = {
+    dialect: 'mysql',
+    database: 'vue_shop',
+    username: 'root',
+    password: '',
+    host: 'localhost',
+    port: 3306,
+};
+
+exports.redis = {
+    url:'redis://localhost:6379'
+};
+
+exports.geetest = {
+    geetest_id: 'geetest_id',
+    geetest_key: 'geetest_key'
+}
+
+exports.sms = {
+    access_key : 'access_key'
+};
